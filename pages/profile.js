86ea7467ff28bc30d9f610bb74142144dc040acc
Joss Doebler <mail@joss-doebler.de>
2020-12-08 15:26:12 +0100

    Add MongoDB/mongoose boilerplate set-up

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,9 @@
 import { useUser } from '@auth/hooks';
 import Layout from '@components/Layout';
+import dbConnect from '@utils/dbConnect';
+import Item from '@models/Item';
 
-const Profile = () => {
+const Profile = ({ items }) => {
   const user = useUser({ redirectTo: '/login' });
 
   return (
@@ -11,10 +13,30 @@ const Profile = () => {
         <>
           <p>Your session:</p>
           <pre>{JSON.stringify(user, null, 2)}</pre>
+          <p>Items:</p>
+          <ul>
+            {items.map((item) => (
+              <li key={item._id}>{item.name}</li>
+            ))}
+          </ul>
         </>
       )}
     </Layout>
   );
 };
+
+export async function getServerSideProps() {
+  await dbConnect();
+
+  /* find all the data in our database */
+  const result = await Item.find({});
+  const items = result.map((doc) => {
+    const item = doc.toObject();
+    item._id = item._id.toString();
+    return item;
+  });
+
+  return { props: { items } };
+}
 
 export default Profile;
